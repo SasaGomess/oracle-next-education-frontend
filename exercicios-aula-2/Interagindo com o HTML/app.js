d1a7b6e3c2020a1b1fb7e376c8125f3756fce72a
SasaGomes <sabrina.gomess.dev@gmail.com>
2025-08-28 18:33:35 -0300

    feat: exercicios funçoes javascript

--- exercicios-aula-2/Interagindo com o HTML/app.js
+++ exercicios-aula-2/Interagindo com o HTML/app.js
@@ -23,3 +23,55 @@ function soma(){
     let soma = sum1 + sum2;
     alert(`A soma é: ${soma}`)
 }
+//Exercício 1 - Crie uma função que calcule o índice de massa corporal (IMC) de uma pessoa, a partir de sua altura, em metros, e peso, em quilogramas, que serão recebidos como parâmetro.
+
+function calculoImc(altura, massa) {
+    return massa / altura;
+}
+
+calculoImc(1.70, 70);
+
+//Exercício 2 - Crie uma função que calcule o valor do fatorial de um número passado como parâmetro.
+function fatorial(numero) {
+    let numeroMultiplicar = numero - 1;
+    let resultado = 0;
+    while(numeroMultiplicar >= 1){
+        resultado =+ numero * numeroMultiplicar;
+        numeroMultiplicar--;
+    }
+    return resultado;
+}
+fatorial(5);
+//Exercicio 3 - Crie uma função que converte um valor em dólar, passado como parâmetro, e retorna o valor equivalente em reais. Para isso, considere a cotação do dólar igual a R$4,80.
+
+function conversorMoedas(dolar) {
+    return dolar * 5.41;
+}
+conversorMoedas(20);
+
+// Exercicio 4 - Crie uma função que mostre na tela a área e o perímetro de uma sala retangular, utilizando altura e largura que serão dadas como parâmetro.
+function calculoPerimetroSala(altura, largura) {
+    let alturaAoQuadrado = Math.pow(altura);
+    let larguraAoQuadrado = Math.pow(largura);
+
+    return alturaAoQuadrado + larguraAoQuadrado;
+}
+calculoPerimetroSala(5, 6);
+
+// Exercicio 5 - Crie uma função que mostre na tela a área e o perímetro de uma sala circular, utilizando seu raio que será fornecido como parâmetro. Considere Pi = 3,14.
+
+function calculoPerimetroCirculo(raio){
+    return raio * 3.14;
+}
+calculoPerimetroCirculo(10);
+
+// Exercicio 6 - Crie uma função que mostre na tela a tabuada de um número dado como parâmetro.
+
+function tabuada(numero) {
+    for (let index = 1; index <= numero; index++) {
+        let resultado = numero * index;
+        console.log(`${numero} X ${index} = ${resultado}`)
+    }
+}
+
+tabuada(7);
