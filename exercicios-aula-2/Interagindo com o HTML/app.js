fa01fe273334351b95fda0a947f77852079b8813
SasaGomes <sabrina.gomess.dev@gmail.com>
2025-08-29 09:12:40 -0300

    feat: add exercises about manipulation of arrays

--- exercicios-aula-2/Interagindo com o HTML/app.js
+++ exercicios-aula-2/Interagindo com o HTML/app.js
@@ -73,5 +73,25 @@ function tabuada(numero) {
         console.log(`${numero} X ${index} = ${resultado}`)
     }
 }
-
 tabuada(7);
+
+// Exercicio 1 - Crie uma lista vazia, com o nome listaGenerica.
+let listaGenerica = [];
+
+// Exercicio 2 - Crie uma lista de linguagens de programação chamada linguagensDeProgramacao com os seguintes elementos: 'JavaScript','C','C++', 'Kotlin' e 'Python'.
+let listaLinguagensP = ['JavaScript','C','C++', 'Kotlin', 'Python'];
+
+// Exercicio 3 - Adicione à lista linguagensDeProgramacao os seguintes elementos: 'Java', 'Ruby' e 'GoLang'.
+listaLinguagensP.push('Java');
+listaLinguagensP.push('Ruby');
+listaLinguagensP.push('Golang');
+
+// Exercicio 4 - Crie uma lista com 3 nomes e exiba no console apenas o primeiro elemento.
+let nomes = ['Sabrina', 'Gabriela', 'Julia'];
+console.log(nomes[0]);
+
+// Exercicio 5 - Crie uma lista com 3 nomes e exiba no console apenas o segundo elemento.
+console.log(nomes[1]);
+
+// Exercicio 6 - Crie uma lista com 3 nomes e exiba no console apenas o último elemento.
+console.log(nomes.length - 1);
--- projeto-lista-funcoes-aula-2/app.js
+++ projeto-lista-funcoes-aula-2/app.js
@@ -1,11 +1,71 @@
-let titulo = document.querySelector('h1');
+let listaNumerosSorteados = [];
+let numeroLimite = 10;
+let numeroSecreto = gerarAleatorio();
+let tentativas = 1;
 
-titulo.innerHTML = 'Jogo do numero secreto';
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
 
-let paragrafo = document.querySelector('p');
+function exibirMensagemIncial() {
+    exibirTextoNaTela('h1', 'Jogo do numero secreto');
+    exibirTextoNaTela('p', 'Escolha um numero entre 1 e 10');
+}
 
-paragrafo.innerHTML = 'Escolha um numero entre 1 e 10';
+exibirMensagemIncial();
 
-function verificarChute(){
-    console.log("O botão foi clicado");
+function verificarChute() {
+    let chute = document.querySelector('input').value;
+
+    if (chute == numeroSecreto) {
+        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+        exibirTextoNaTela('h1', 'Parabéns você acertou!');
+
+        let mensagemTentativas = `Você descobriu numero secreto com ${tentativas} ${palavraTentativa}.`
+        exibirTextoNaTela('p', mensagemTentativas);
+
+       document.getElementById('reiniciar').removeAttribute('disabled');
+
+    } else if (chute > numeroSecreto) {
+        
+        exibirTextoNaTela('p', 'O numero secreto é menor');
+
+    } else {
+
+        exibirTextoNaTela('p', 'O numero secreto é maior');
+
+    };
+    tentativas++;
+    limparCampo();
+}
+
+function gerarAleatorio() {
+    let numeroEscolhido =  parseInt(Math.random() * numeroLimite + 1);
+    let quantidadeDeElementosNaLista = listaNumerosSorteados.length;
+
+    if(quantidadeDeElementosNaLista == numeroLimite){
+        listaNumerosSorteados = [];
+    }
+
+    if(listaNumerosSorteados.includes(numeroEscolhido)) {// includes metodo especifico do javascript
+        return gerarAleatorio();
+    } else{
+        listaNumerosSorteados.push(numeroEscolhido)
+        return numeroEscolhido;
+    }
+}
+
+
+function limparCampo() {
+    // Pegando o input em si para manipular o valor de dentro. Por isso não foi pega a variavel chute que so recebe o valor de dentro do input
+    chute = document.querySelector('input');
+    chute.value = '';
+}
+function reiniciar() {
+    numeroSecreto = gerarAleatorio();
+    limparCampo();
+    tentativas = 1;
+    exibirMensagemIncial()
+    document.getElementById('reiniciar').setAttribute('disabled', true)
 }
